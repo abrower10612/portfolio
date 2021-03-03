3fff3e73d471ae4e83d11a5ac9304941f9330202
abrower10612 <abrower7@byui.edu>
2021-03-03 11:21:16 -0700

    adding left border white to sidenav

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,11 @@
-function myFunction(x) {
-  x.classList.toggle("change");
+function openNav(x) {
+  // x.classList.toggle("change");
+  mySidenav.classList.toggle("change");
   document.getElementById("mySidenav").style.width = "175px";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  mySidenav.classList.toggle("change");
 }
