eccb2efc90a9560e945c07442668323817364fec
abrower10612 <abrower7@byui.edu>
2021-03-03 10:42:09 -0700

    adding side menu when menu icon is clicked

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,10 @@
 function myFunction(x) {
   x.classList.toggle("change");
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("container").style.opacity = "100%";
 }
