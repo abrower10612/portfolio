ba270828265903f549aaa9b62dae67e2b82f3cdc
abrower10612 <abrower7@byui.edu>
2021-03-03 11:15:00 -0700

    asdf

--- js/menu.js
+++ js/menu.js
@@ -6,5 +6,4 @@ function myFunction(x) {
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("container").style.opacity = "100%";
 }
